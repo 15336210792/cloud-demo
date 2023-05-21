5bdd9d0cd6c02f0dcf6cf38d3e9e2efc65451bb2
zzh <zzh@123.com>
2023-05-21 17:04:56 +0800

    nacos之前的结束

--- cloud-demo/order-service/src/main/java/cn/itcast/order/OrderApplication.java
+++ cloud-demo/order-service/src/main/java/cn/itcast/order/OrderApplication.java
@@ -5,17 +5,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestTemplate;
 
+
 @MapperScan("cn.itcast.order.mapper")
+//@ComponentScan("cn.itcast.order")
 @SpringBootApplication
 public class OrderApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OrderApplication.class, args);
+
     }
-
-
     @Bean
     @LoadBalanced  //负载均衡的注解
     public RestTemplate restTemplate(){
